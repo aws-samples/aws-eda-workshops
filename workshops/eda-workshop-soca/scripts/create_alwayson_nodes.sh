92ed168b3e284fa9172ec22bc64e278037f770e1
ahmedelz <ahmedelz@users.noreply.github.com>
2021-01-22 15:20:17 -0800

    Update create_alwayson_nodes.sh

--- workshops/eda-workshop-soca/scripts/create_alwayson_nodes.sh
+++ workshops/eda-workshop-soca/scripts/create_alwayson_nodes.sh
@@ -14,7 +14,9 @@ AMI_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/la
 MAC_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs)
 SUBNET_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC_ID/subnet-id)
 
-eval $CMD_PREFIX "/apps/soca/$SOCA_CONFIGURATION/python/latest/bin/python3 /apps/soca/$SOCA_CONFIGURATION/cluster_manager/add_nodes.py \
-    --ht_support true --instance_ami $AMI_ID --base_os \"centos7\" --root_size 10 --subnet_id $SUBNET_ID \
-    --instance_type t3.large --desired_capacity 1 --queue alwayson --job_owner $USER --job_name $USER \
+eval $CMD_PREFIX "/apps/soca/$SOCA_CONFIGURATION/python/latest/bin/python3 \
+    /apps/soca/$SOCA_CONFIGURATION/cluster_manager/add_nodes.py \
+    --ht_support true --instance_ami $AMI_ID --base_os \"centos7\" \
+    --root_size 10 --subnet_id $SUBNET_ID --instance_type t3.large \
+    --desired_capacity 1 --queue alwayson --job_owner $USER --job_name $USER \
     --keep_forever false --terminate_when_idle 10"
