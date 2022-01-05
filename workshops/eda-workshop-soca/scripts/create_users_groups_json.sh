efde656c8090b953ebefb272a753f4745319168d
Allan Carter <cartalla@amazon.com>
2022-01-05 23:39:37 +0000

    Add execute permissions to scripts

--- workshops/eda-workshop-soca/scripts/create_users_groups_json.sh
+++ workshops/eda-workshop-soca/scripts/create_users_groups_json.sh
@@ -7,5 +7,12 @@ fi
 cluster_name=$1
 scripts_dir=$2
 
+if [[ ":$cluster_name" == ":" ]]; then
+    $cluster_name=$(basename /apps/soca/soca-*)
+fi
+if [[ ":$scripts_dir" == ":" ]]; then
+    $scripts_dir=/apps/soca/$cluster_name/cluster_node_bootstrap
+fi
+
 cd ../ansible
 ansible-playbook create_users_groups_json.yml -i inventories/local.yml -e ClusterName=$cluster_name -e ScriptsDir=$scripts_dir
