8255ffe8de845903148d94d2ea2f60b02c56d1e5
ahmedelz <ahmedelz@users.noreply.github.com>
2022-08-24 16:13:31 -0700

    Update soca_custom_ami_imagebuilder.sh

--- workshops/eda-workshop-soca/scripts/soca_custom_ami_imagebuilder.sh
+++ workshops/eda-workshop-soca/scripts/soca_custom_ami_imagebuilder.sh
@@ -6,6 +6,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+mkdir -p /root/bootstrap
 exec > >(tee /root/bootstrap/idea_preinstalled_packages.log ) 2>&1
 
 OS_NAME=`awk -F= '/^NAME=/{print $2}' /etc/os-release`
