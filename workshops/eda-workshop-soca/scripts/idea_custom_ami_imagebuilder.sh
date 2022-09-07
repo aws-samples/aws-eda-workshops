d8e5b4d926b3924222504de1dcce7437f98edd81
ahmedelz <ahmedelz@users.noreply.github.com>
2022-09-06 18:13:33 -0700

    Update idea_custom_ami_imagebuilder.sh

--- workshops/eda-workshop-soca/scripts/idea_custom_ami_imagebuilder.sh
+++ workshops/eda-workshop-soca/scripts/idea_custom_ami_imagebuilder.sh
@@ -12,7 +12,7 @@ exec > >(tee /root/bootstrap/idea_preinstalled_packages.log ) 2>&1
 source /etc/os-release
 if [[ "$NAME" == "\"Red Hat Enterprise Linux Server\"" ]] && [[ "${VERSION_ID}" == \"7.9\" ]]; then
     OS="rhel7"
-elif [[ "$NAME" == "\"CentOS Linux\"" ]] && [[ "${VERSION_ID}" == "7" ]]; then
+elif [[ "$NAME" == "CentOS Linux" ]] && [[ "${VERSION_ID}" == "7" ]]; then
     OS="centos7"
 elif [[ "$NAME" == "\"Amazon Linux\"" ]] && [[ "${VERSION_ID}" == "\"2\"" ]]; then
     OS="amazonlinux2"
